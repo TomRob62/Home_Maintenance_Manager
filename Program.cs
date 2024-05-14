01b3e58ab8062396b11851786e75d24a24cd3411
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-05-13 22:52:03 -0400

    feat testing hello world

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿/**
+Thomas Roberts
+May 13, 2024
+
+Description: Main program.
+*/
+
+namespace HomeMaintenanceManager
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
